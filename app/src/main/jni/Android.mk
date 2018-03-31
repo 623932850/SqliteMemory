833fdb83d61807cc42355e07231de6eff6c620f6
xiangjianhua <xiangjianhua@auteltech.net>
2018-03-31 18:02:25 +0800

    udpate

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,4 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:=test.cpp
 LOCAL_MODULE:=hello
+LOCAL_C_INCLUDES:=F:/AndroidSDK/ndk-bundle/platforms/android-21/arch-arm64/usr/include
+LOCAL_LDLIBS:=-LF:/AndroidSDK/ndk-bundle/platforms/android-21/arch-arm64/usr/lib -lsqlite
+LOCAL_SHARED_LIBRARIES:=libsqlite
 include $(BUILD_SHARED_LIBRARY)
